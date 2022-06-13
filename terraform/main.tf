6658033705bdbdab21b96bc9d306450e4946c3c5
isanasan <yfcgpsebp@gmail.com>
2022-06-13 15:22:48 +0900

    update: add resources BigQuery Dataset ^& tabale

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+
+variable project_id {
+  default = "<your-project-id>"
+}
+
 terraform {
   required_providers {
     google = {
@@ -17,20 +22,22 @@ terraform {
 }
 
 provider "google" {
-  credentials = "${file("solar-attic-352013-9a8671f4f33f.json")}"
-  project     = "solar-attic-352013"
+  project = var.project_id
   region      = "asia-northeast1"
 }
 
 resource "google_service_account" "github_importer" {
   account_id   = "github-importer"
-  display_name = "github-importer"
   description  = "GitHubの情報をBigQueryに流し込むためのサービスアカウント"
+  display_name = "github-importer"
+  project = var.project_id
 }
+
 resource "google_project_iam_member" "github_importer_bigquery_jobuser_bindings" {
   role   = "roles/bigquery.jobUser"
   member = "serviceAccount:${google_service_account.github_importer.email}"
 }
+
 resource "google_bigquery_dataset_access" "github_importer_bigquery_dataset_access_bindings" {
   dataset_id    = "source__github"
   role          = "WRITER"
@@ -40,8 +47,40 @@ resource "google_bigquery_dataset_access" "github_importer_bigquery_dataset_acce
 resource "google_service_account" "dataform_executor" {
   account_id  = "dataform-executor"
   description = "Datafrom (dataform.co) に付与する用のサービスアカウント"
+  project = var.project_id
 }
+
 resource "google_project_iam_member" "dataform_executor_bigquery_admin_bindings" {
   role   = "roles/bigquery.admin"
   member = "serviceAccount:${google_service_account.dataform_executor.email}"
 }
+
+resource "google_bigquery_dataset" "source__github" {
+  access {
+    role          = "OWNER"
+    special_group = "projectOwners"
+  }
+
+  access {
+    role          = "READER"
+    special_group = "projectReaders"
+  }
+
+  access {
+    role          = "WRITER"
+    special_group = "projectWriters"
+  }
+
+  dataset_id                 = "source__github"
+  delete_contents_on_destroy = false
+  location                   = "asia-northeast1"
+  project = var.project_id
+}
+
+resource "google_bigquery_table" "pull_requests" {
+  dataset_id = google_bigquery_dataset.source__github.dataset_id
+  project = var.project_id
+  schema     = "[{\"mode\":\"REPEATED\",\"name\":\"labelNames\",\"type\":\"STRING\"},{\"mode\":\"NULLABLE\",\"name\":\"headRefName\",\"type\":\"STRING\"},{\"mode\":\"NULLABLE\",\"name\":\"baseRefName\",\"type\":\"STRING\"},{\"mode\":\"NULLABLE\",\"name\":\"deletions\",\"type\":\"INTEGER\"},{\"fields\":[{\"mode\":\"NULLABLE\",\"name\":\"typename\",\"type\":\"STRING\"},{\"mode\":\"NULLABLE\",\"name\":\"login\",\"type\":\"STRING\"}],\"mode\":\"NULLABLE\",\"name\":\"author\",\"type\":\"RECORD\"},{\"description\":\"bq-datetime\",\"mode\":\"NULLABLE\",\"name\":\"firstCommittedAt\",\"type\":\"TIMESTAMP\"},{\"mode\":\"NULLABLE\",\"name\":\"id\",\"type\":\"STRING\"},{\"mode\":\"NULLABLE\",\"name\":\"additions\",\"type\":\"INTEGER\"},{\"fields\":[{\"mode\":\"NULLABLE\",\"name\":\"totalCount\",\"type\":\"INTEGER\"}],\"mode\":\"NULLABLE\",\"name\":\"reviews\",\"type\":\"RECORD\"},{\"description\":\"bq-datetime\",\"mode\":\"NULLABLE\",\"name\":\"mergedAt\",\"type\":\"TIMESTAMP\"},{\"fields\":[{\"mode\":\"NULLABLE\",\"name\":\"nameWithOwner\",\"type\":\"STRING\"}],\"mode\":\"NULLABLE\",\"name\":\"repository\",\"type\":\"RECORD\"},{\"mode\":\"NULLABLE\",\"name\":\"number\",\"type\":\"INTEGER\"},{\"mode\":\"NULLABLE\",\"name\":\"title\",\"type\":\"STRING\"},{\"description\":\"bq-datetime\",\"mode\":\"NULLABLE\",\"name\":\"createdAt\",\"type\":\"TIMESTAMP\"},{\"mode\":\"NULLABLE\",\"name\":\"url\",\"type\":\"STRING\"}]"
+  table_id   = "pull_requests"
+}
+
